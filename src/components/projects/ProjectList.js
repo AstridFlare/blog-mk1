659c07adb16909cc90eae4f656603fad6719542c
AstridFlare <AstridFlare@Gmail.com>
2019-03-06 19:26:16 -0800

    initial commit

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ProjectSummary from './ProjectSummary';
+
+export default () => {
+	return (
+		<div className="project-list section">
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
+		</div>
+	);
+};
